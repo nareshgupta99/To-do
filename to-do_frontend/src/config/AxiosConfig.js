d50a2b55fba59c4fb71bcc5ffcd78189c9ba0b37
Naresh Gupta <nareshgupta0899@gmail.com>
2025-02-12 06:11:19 +0530

    updated

--- to-do_frontend/src/config/AxiosConfig.js
+++ to-do_frontend/src/config/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL= process.env.REACT_APP_BASE_URL || "http://localhost:8080/api/v1"
+const baseURL= process.env.REACT_APP_BASE_URL || "http://localhost:3333/api/v1"
 
 const PublicAxios = axios.create({
     baseURL: baseURL,
--- to-dolist/server.js
+++ to-dolist/server.js
@@ -13,6 +13,8 @@ const app=express();
 
 const PORT= process.env.PORT || 8082
 
+const LOCAL_IP = process.env.LOCAL_IP ;
+
 const CORSORIGIN=process.env.CORS;
 
 
@@ -20,6 +22,7 @@ const CORSORIGIN=process.env.CORS;
 
 const corsOptions = {
     origin: CORSORIGIN, // Replace with your client domain
+    origin:"*",
     optionsSuccessStatus: 200 // Some legacy browsers choke on 204
   };
 
@@ -36,15 +39,27 @@ app.use("/api/v1/user",require("./routers/UserRoutes"));
 
 // app.use("/api/v1/user",require(""));
 
-mongoose.connect("mongodb://localhost:27017/to-do").then(()=>{
+// mongoose.connect("mongodb://localhost:27017/to-do").then(()=>{
+//     console.log("db is connected")
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+
+
+mongoose.connect("mongodb+srv://naresh:GF16QeEvGyy377PG@flutter.31o5v.mongodb.net/?retryWrites=true&w=majority&appName=flutter").then(()=>{
     console.log("db is connected")
 }).catch((err)=>{
     console.log(err)
 })
 
+app.get("/hello",(req,res)=> res.send({
+    name:"naresh"
+}))
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+
+app.listen(3333,LOCAL_IP,()=>{
+    console.log(`Server running on ${PORT}`);
 })
 
 
